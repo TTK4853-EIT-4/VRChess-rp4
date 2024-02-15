923e4d2ee0366ce3518d7075567f6376afb6ab8a
Iver <iverrolv@hotmail.com>
2024-02-15 15:16:17 +0100

    Added pinout file

---
+++ pinout.py
@@ -0,0 +1,23 @@
+### Pins defined using board pins:
+# NAME          = Board pin  # GPIO# / Other 
+LEDSTRIP        = "BOARD12"  # GPIO18 / PWM0 
+MOTOR_ENABLE    = "BOARD12"  # GPIO27
+M1IN1           = "BOARD15"  # GPIO22
+M1IN2           = "BOARD16"  # GPIO23
+M1IN3           = "BOARD7"   # GPIO4 / GCLK0
+M1IN4           = "BOARD29"  # GPIO5 / GCLK1
+M2IN1           = "BOARD31"  # GPIO6 / GCLK2
+M2IN2           = "BOARD26"  # GPIO7 / ~CE1
+M2IN3           = "BOARD24"  # GPIO8 / ~CE0
+M2IN4           = "BOARD21"  # GPIO9 / MISO0
+END_DET1        = "BOARD18"  # GPIO24
+END_DET2        = "BOARD22"  # GPIO25
+BTN_JOIN        = "BOARD36"  # GPIO16
+BTN_EXTRA       = "BOARD37"  # GPIO26
+
+### Other pins:
+MISO1           = "BOARD35"  # GPIO19
+MOSI1           = "BOARD38"  # GPIO20
+SCLK1           = "BOARD40"  # GPIO21
+RX              = "BOARD8"   # GPIO14
+TX              = "BOARD10"  # GPIO15
