999f294a882dcdbefa2bdfcc355af475fd38462a
Iver <iverrolv@hotmail.com>
2024-02-15 14:29:54 +0100

    test setup

---
+++ main.py
@@ -0,0 +1,14 @@
+class Superman:
+
+    def __init__(self, **kwargs) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "SUPERHOT"
+    
+def main():
+    idk = Superman(jesus_christ="it's jason bours")
+    print(str(idk))
+
+if __name__ == '__main__':
+    main()
