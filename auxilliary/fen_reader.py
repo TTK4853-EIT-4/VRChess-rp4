11bb54f06586bfaceb14337617077a711315571c
Iver Rolvsjord <iverrolv@hotmail.com>
2024-02-28 14:28:33 +0100

    direct path + start edge

--- auxilliary/fen_reader.py
+++ auxilliary/fen_reader.py
@@ -17,13 +17,8 @@ Closed: A move along a path of squares that contains one or more pieces, and mus
 '''
 # ASSUMES servers knows board state. 
 
-COLUMN = ["A", "B", "C", "D", "E", "F", "G", "H"]
-ROW = [1, 2, 3, 4, 5, 6, 7, 8]
-
-# TODO: Parse FEN string
-# TODO: If "open" route, give open route.
-# TODO: If no open route possible, calculate a closed route.
 # TODO: Note that only "special moves" such as castling, graveyard and horses require "threading" the pieces
+########################### FEN:
 class FEN_Reader:
     # Prob not needed, delete.
     _current_board_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
@@ -38,6 +33,24 @@ def parse_fen(chess_fen: str):
     fullmove = int(fullmove)
     return board, move, castling, passant, halfmove, fullmove
     
+########################## END FEN
+
+########################## PATH:
+
+# Move identifiers
+HORSE = "horse"
+CASTLING = "castling"
+GRAVEYARD = "graveyard"
+D2G = "distance2graveyard"
+
+COLUMN = ["@", "A", "B", "C", "D", "E", "F", "G", "H", "I"]
+ROW = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+COLUMN_OFFSET = 1
+ROW_OFFSET = 2
+
+
+#COLUMN = ["A", "B", "C", "D", "E", "F", "G", "H"]
+#ROW = [1, 2, 3, 4, 5, 6, 7, 8]
 
 def check_for_open_path(board: str, old_pos: str, new_pos: str):
     """Check if there is an open path from current position to new position.
@@ -66,8 +79,10 @@ def check_for_open_path(board: str, old_pos: str, new_pos: str):
         path = get_displaced_path(old_pos, new_pos)
     return open_path
 
+
 def get_direct_path(old_coord: tuple[int, int], new_coord: tuple[int, int], is_diagonal: bool):
     """Assumes no blocking pieces in path. Does not include position of self.
+    Assumes that path is either a diagonal or a straight run.
 
     Args:
         old_coord (tuple[int, int]): _description_
@@ -79,47 +94,95 @@ def get_direct_path(old_coord: tuple[int, int], new_coord: tuple[int, int], is_d
     """
     if is_diagonal:
         letters = [COLUMN[i] for i in range(old_coord[0], new_coord[0])]
-        numbers = [ROW[i] for i in range(old_coord[1], new_coord[1])]
+        numbers = [ROW[i]-ROW_OFFSET+1 for i in range(old_coord[1], new_coord[1])]
     else:  # straight
         if old_coord[0] - new_coord[0] == 0: # straight along letter axis
             letters = [COLUMN[old_coord[0]-1] for _ in range(old_coord[1], new_coord[1])]
-            numbers = [ROW[i] for i in range(old_coord[1], new_coord[1])]
+            numbers = [ROW[i]-ROW_OFFSET+1 for i in range(old_coord[1], new_coord[1])]
         elif old_coord[1] - new_coord[1] == 0: # straight along number axis
             letters = [COLUMN[i] for i in range(old_coord[0], new_coord[0])]
-            numbers = [ROW[old_coord[1]-1] for _ in range(old_coord[0], new_coord[0])]
+            numbers = [ROW[old_coord[1]-ROW_OFFSET] for _ in range(old_coord[0], new_coord[0])]
             
     path = [(letters[i], numbers[i]) for i in range(len(letters))]
     # If self is needed, uncomment line:
     # path = coords_to_position(old_coord) + path
     return path
     
-def get_displaced_path(old_pos: str, new_pos: str):
+def get_displaced_path(old_coord: tuple[int, int], new_coord: tuple[int, int] | str, special_identifier: str):
     # Get the displace path of a special move.
-    if new_pos == "graveyard":
-        # find free graveyard spot.
+    
+    # Line defined as: B5B6, the line between B5 and B6.
+    # Edge case: B8B9, the line between B6 and out of bounds.
+    old_pos = coords_to_position(old_coord)
+    new_pos = coords_to_position(new_pos)
+    
+    if special_identifier == GRAVEYARD:  # either old or new is in graveyard. Old if pawn switch to new piece
+        # TODO:
+        # find path to edge
+        # add distance 2 graveyard when passing over "@" or "I".
+        # find path to spot
+        
+        
+        # Path will look somewhat like: [2Edge, edge1, edge2, edge3, dist2grave, edge3, 2Spot]
+        
         print("The piece was yoted")
-    else:  # new_pos is actual location
-        print("Horse, castling etc...")
-    
-    
+        
+    # horse and castling is the same in the eyes of this algo....
+    elif special_identifier == HORSE or special_identifier == CASTLING:  # new_pos is actual location
+        pass
     path = ""
     return path
 
 
+def start_edgerun(start_coord, end_coord):
+    """Find the best edgepoint to start at.
+    Assume that best start point is always closest to end point
+
+    Returns:
+        _type_: _description_
+    """
+    dx = abs(start_coord[0] - end_coord[0])
+    dy = abs(start_coord[1] - end_coord[1])
+    edge = []
+    if dx >= dy:  # prioritize horizontal
+        if start_coord[1] - end_coord[1] >= 0: # end lower
+            # "move to upper edge"
+            up = (start_coord[0], start_coord[1]+1)
+            edge = [start_coord, up]
+        elif start_coord[1] - end_coord[1] < 0: # end higher
+            # "move to lower edge"
+            down = (start_coord[0], start_coord[1]-1)
+            edge = [start_coord, down]
+    else:  # prioritize vertical
+        if start_coord[0] - end_coord[0] >= 0: # end left
+            # "move to left edge"
+            left = (start_coord[0]-1, start_coord[1])
+            edge = [start_coord, left]
+        elif start_coord[0] - end_coord[0] < 0: # end right
+            # "move to right edge"
+            right = (start_coord[0]+1, start_coord[1])
+            edge = [start_coord, right]
+    return edge
+        
+    
+    
+
+########### HELPER FUNCS:
+
 def position_to_coords(pos: str):
-    """A1 = (1, 1), H8 = (8, 8) 
+    """ 
 
     Args:
         pos (str): _description_
     """
-    col = ord(pos[0].upper()) - ord('A') + 1
-    row = int(pos[1])
+    col = ord(pos[0].upper()) - ord('A') + 1 + COLUMN_OFFSET
+    row = int(pos[1]) + ROW_OFFSET
     return (col, row)
 
 
 def coords_to_position(coord: tuple[int, int]):
-    col = chr(coord[0]-1)
-    row = coord[1]
+    col = chr(coord[0]-1- COLUMN_OFFSET)
+    row = coord[1] - ROW_OFFSET
     return col + repr(row)
     
 
@@ -133,5 +196,21 @@ def get_open_distance(coord1, coord2) -> tuple[int, bool]:
         diagonal = True
     return total, diagonal
     
+############## END PATH
     
     
+#        |d| : distance between board and grave       
+#   Grave|d|Board for drawing "edges"|d|    
+#    0 1  2 3 4 5 6 7 8 9 10 11 12 13
+#    > ?  @ A B C D E F G H  I  J  K
+#         9 
+#         8 
+#         7 
+#         6 
+#         5 
+#         4 
+#         3 
+#         2 
+#         1 
+#         0 
+#   
