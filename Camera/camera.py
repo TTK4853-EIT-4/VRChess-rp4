f23306feec0683f8dc9e5746e14e28b0d21d5942
Iver Rolvsjord <iverrolv@hotmail.com>
2024-03-13 13:54:39 +0100

    camera controller

--- Camera/camera.py
+++ Camera/camera.py
@@ -1,13 +1,45 @@
 print('camera testing 1')
 from picamera2 import Picamera2
 from libcamera import controls
-from time import sleep
+from time import sleep  
 from uuid import uuid4
+from auxilliary.fen_reader import matrix2fen
 
 PICS_FOLDER = "PICS/"
 
 #picam2.start(show_preview=True)
 #picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
+class CameraController:
+    def __init__(self) -> None:
+        self.cam = initialize_camera()
+        self.picture = ""
+        self.cv_board = [["" for _ in range(8)] for _ in range(8)]
+        
+    def analyze_board(self) -> str:
+        """Analyze current picture and return a FEN string of the current board.
+
+        Returns:
+            str: FEN string, current board
+        """
+        self._take_picture()
+        self._computer_vision()
+        return self._construct_board()
+    
+    def _take_picture(self):
+        self.picture = send_nudes(self.cam)
+    
+    # TODO:
+    def _computer_vision(self):
+        """Do all the computer vision stuff"""
+        self.cv_board = self.cv_board
+        return
+    
+    def _construct_board(self) -> str:
+        """Construct board as fen str from cv_board"""
+        return matrix2fen(self.cv_board)
+    
+
 
 def initialize_camera() -> Picamera2:
 	cam = Picamera2()
