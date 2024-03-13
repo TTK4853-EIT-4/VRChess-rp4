7d10ac7e69463c992f47207d426d6eaa42afba0d
Iver <iverrolv@hotmail.com>
2024-03-13 12:41:42 +0100

    init camera and take still

--- Camera/camera.py
+++ Camera/camera.py
@@ -1,1 +1,32 @@
-print('camera')
+print('camera testing 1')
+from picamera2 import Picamera2
+from libcamera import controls
+from time import sleep
+from uuid import uuid4
+
+PICS_FOLDER = "PICS/"
+
+#picam2.start(show_preview=True)
+#picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
+def initialize_camera() -> Picamera2:
+	cam = Picamera2()
+	cfg = cam.create_still_configuration(main={"size": (4608, 2592)})
+	cam.configure(cfg)
+	return cam
+
+
+def send_nudes(cam: Picamera2) -> str:
+	global PICS_FOLDER
+	cam.start()
+	unique_filename = PICS_FOLDER + str(uuid4())+ ".jpg"
+	cam.capture_file(unique_filename)
+	cam.stop()
+	return unique_filename
+
+
+if __name__ == '__main__':
+	pc2 = initialize_camera()
+	pic = send_nudes(pc2)
+	print(f"New pic stored at {pic}")
+	
