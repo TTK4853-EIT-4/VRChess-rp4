f3e40263024caab0a61197ae635c63b33f2bcc01
Casper Faarlund <caspef@ntnu.no>
2024-03-20 14:16:06 +0100

    cvtest

---
+++ Camera/cvtest.py
@@ -0,0 +1,32 @@
+
+print('camera testing 1')
+from picamera2 import Picamera2
+from libcamera import controls
+from time import sleep  
+from uuid import uuid4
+from auxilliary.fen_reader import matrix2fen
+
+PICS_FOLDER = "PICS/"
+
+
+def initialize_camera() -> Picamera2:
+	cam = Picamera2()
+	cfg = cam.create_still_configuration(main={"size": (4608, 2592)})
+	cam.configure(cfg)
+	return cam
+
+
+def send_nudes(cam: Picamera2) -> str:
+	global PICS_FOLDER
+	cam.start()
+	unique_filename = PICS_FOLDER + str(uuid4())+ ".jpg"
+	cam.capture_file(unique_filename)
+	cam.stop()
+	return unique_filename
+
+
+if __name__ == '__main__':
+	pc2 = initialize_camera()
+	pic = send_nudes(pc2)
+	print(f"New pic stored at {pic}")
+	
