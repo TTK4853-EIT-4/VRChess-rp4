7cebf71ff6b099b24a6398e913b4d2453cf91d69
Iver Rolvsjord <iverrolv@hotmail.com>
2024-04-10 10:30:26 +0200

    newcrop moved to camera

--- Camera/camera.py
+++ Camera/camera.py
@@ -5,6 +5,8 @@ from time import sleep
 from uuid import uuid4
 from auxilliary.fen_reader import matrix2fen
 from PIL import Image, ImageDraw
+import cv2
+import numpy as np
 # import the inference-sdk
 from inference_sdk import InferenceHTTPClient
 
@@ -32,6 +34,7 @@ class CameraController:
     
     def _take_picture(self):
         self.picture = send_nudes(self.cam)
+        newcrop(self.picture)
     
     def _computer_vision(self):
         """Do all the computer vision stuff"""
@@ -750,6 +753,50 @@ def send_nudes(cam: Picamera2) -> str:
 	cam.capture_file(unique_filename)
 	cam.stop()
 	return unique_filename
+
+
+def newcrop(img):
+    img = cv2.imread(img)
+    # points for test.jpg
+    cnt = np.array([
+            [[1230, 480]],
+            [[3410, 329]],
+            [[3471, 2486]],
+            [[1229, 2485]]
+        ])
+    # print("shape of cnt: {}".format(cnt.shape))
+    rect = cv2.minAreaRect(cnt)
+    # print("rect: {}".format(rect))
+
+    # the order of the box points: bottom left, top left, top right,
+    # bottom right
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+
+    # print("bounding box: {}".format(box))
+    cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+
+    # get width and height of the detected rectangle
+    width = int(rect[1][0])
+    height = int(rect[1][1])
+
+    src_pts = box.astype("float32")
+    # coordinate of the points in box points after the rectangle has been
+    # straightened
+    dst_pts = np.array([[0, height-1],
+                        [0, 0],
+                        [width-1, 0],
+                        [width-1, height-1]], dtype="float32")
+
+    # the perspective transformation matrix
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+
+    # directly warp the rotated rectangle to get the straightened rectangle
+    warped = cv2.warpPerspective(img, M, (width, height))
+
+    cv2.imwrite("crop_img.jpg", warped)
+    cv2.waitKey(0)
+
 
 
 if __name__ == '__main__':
